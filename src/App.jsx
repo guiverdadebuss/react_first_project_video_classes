7713a713f89ecbc99db590c2ca6f95af607f8b25
guiverdadebuss <guilhermeverdadebuss@gmail.com>
2025-09-22 23:56:33 +0100

    feat: add task and task

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
 
 
 
 function App() {
   return (
     <div>
-
+      <h1>Gerenciador de Tarefas</h1>
+      <AddTask />
+      <Tasks />
     </div>
   );
 }
