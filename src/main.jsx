a1aaa9e62eb390c70999ed5fd9c078c043bbeb50
guiverdadebuss <guilhermeverdadebuss@gmail.com>
2025-09-23 23:24:20 +0100

    feat: adding pages Task Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
 import App from './App';
 import './index.css'; // 👈 Aqui é onde o Tailwind entra
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import TaskPage from './pages/TaskPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: '/task',
+    element: <TaskPage />
+  }
+]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
