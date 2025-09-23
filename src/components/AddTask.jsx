44e627de4309b58fef9291cbaf003d29942aef46
guiverdadebuss <guilhermeverdadebuss@gmail.com>
2025-09-23 22:18:52 +0100

    feat: adding css to the project

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,9 +1,17 @@
 function AddTask() {
   return (
     <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
-      <input type="text" placeholder="Digite o título da tarefa"></input>
-      <input type="text" placeholder="Digite a descrição da tarefa"></input>
-      <button>Adicionar</button>
+      <input
+        type="text"
+        placeholder="Digite o título da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+      />
+      <input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+      />
+      <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">Adicionar</button>
     </div>
   );
 }
