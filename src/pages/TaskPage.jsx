b36c74d2a04247e2a5e7c18d889ba3bc1841a098
guiverdadebuss <guilhermeverdadebuss@gmail.com>
2025-09-25 13:58:44 +0100

    feat: button back task page

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -2,7 +2,7 @@ import { ChevronLeftIcon } from "lucide-react";
 import { useNavigate, useSearchParams } from "react-router";
 
 function TaskPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
@@ -10,9 +10,12 @@ function TaskPage() {
     <div className="h-screen w-screen bg-slate-500 p-6">
       <div className="w-[500px] space-y-4">
         <div className="flex justify-center relative mb-6">
-            <button onClick={() => navigate(-1)} className="absolute left-0 top-0 bottom-0 text-slate-100">
-                <ChevronLeftIcon />
-            </button>
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute left-0 top-0 bottom-0 text-slate-100"
+          >
+            <ChevronLeftIcon />
+          </button>
           <h1 className="text-3xl text-slate-100 font-bold text-center">
             Detalhes da Tarefa
           </h1>
