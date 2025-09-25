d224d2cd54d28093f3a03868e97858e3b2066108
guiverdadebuss <guilhermeverdadebuss@gmail.com>
2025-09-25 13:43:35 +0100

    feat: task page update

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,15 +1,22 @@
-import { useSearchParams } from "react-router"
+import { useSearchParams } from "react-router";
 
 function TaskPage() {
-    const [searchParams] = useSearchParams();
-    const title = searchParams.get("title");
-    const description = searchParams.get("description")
-    return <div className="h-screen w-screen bg-slate-500 p-6">
-        <h1>{title}</h1>
-        <p>{description}</p>
-
-
+  const [searchParams] = useSearchParams();
+  const title = searchParams.get("title");
+  const description = searchParams.get("description");
+  return (
+    <div className="h-screen w-screen bg-slate-500 p-6">
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Detalhes da Tarefa
+        </h1>
+        <div className="bg-slate-200 p-4 rounded-mg">
+          <h1 className="text-xl font-bold text-slate-600">{title}</h1>
+          <p className="text-slate-600">{description}</p>
+        </div>
+      </div>
     </div>
+  );
 }
 
-export default  TaskPage
+export default TaskPage;
